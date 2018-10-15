6cc76364821de9f88665078eb394b991de22fede
MattBrubakerSmith <matthewbrubakersmith@gmail.com>
2018-10-15 15:46:06 -0500

    Added Assignment 2.3

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require("mongoose");
+var apiCatalog = require("./routes/api-catalog");
 
 mongoose.Promise = require("bluebird");
 
@@ -29,6 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use("/api", apiCatalog);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
